e61d34ee0acbc88068cb295f5d3077f9ecdecb5d
Tanya Lyakh <Ttanka1110girl@gmail.com>
2017-03-06 13:08:16 +0100

    Finish project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 
+	var btn = $("#button");
+	var ul = $("ul");
+	
+	btn.on('click', function(e){
+		e.preventDefault();
+		console.log('ok');
+		var txt = $('input[type="text"]').val();
+		console.log(txt);
+		if (txt.length!=0){
+			var li = $('<li>'+txt+'</li>');
+			ul.append(li);
+			$('input[type="text"]').val("");
+		}
+	});
+
 });
